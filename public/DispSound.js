8246a11115fa699e767f7da751fae90ea58999f8
DranoelMit <tim.matthew.leonard@gmail.com>
2018-02-10 18:02:36 -0500

    Fix mobile issues

--- public/DispSound.js
+++ public/DispSound.js
@@ -1,5 +1,17 @@
 
 function moveCube(frequencyData){
 var box = document.getElementById("testCube");
-box.setAttribute("scale", ""+ frequencyData[20]/10 + " " +  frequencyData[40]/10 + " " +  frequencyData[1]/10);
+box.setAttribute("scale", "1" + " 1" +  frequencyData[40]/20 );
 };
+
+
+
+function playMusic(){
+     console.log("reached play");
+     var audios = document.getElementsByTagName('audio');
+
+     for(i=0; i<audios.length;i++){
+     audios[i].play();
+     }
+     document.getElementById('playForm').style.display = "none";
+}
