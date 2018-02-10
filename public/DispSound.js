a7248273b6cd89c807fd5376b053e44e769eac46
DranoelMit <tim.matthew.leonard@gmail.com>
2018-02-10 15:17:57 -0500

    add basic audio rendering

---
+++ public/DispSound.js
@@ -0,0 +1,5 @@
+
+function moveCube(frequencyData){
+var box = document.getElementById("testCube");
+box.setAttribute("scale", ""+ frequencyData[20]/10 + " " +  frequencyData[40]/10 + " " +  frequencyData[1]/10);
+};
---
+++ public/SoundGrabber.js
@@ -0,0 +1,22 @@
+
+window.onload = function() {
+     var ctx = new AudioContext();
+       var audio = document.getElementsByTagName("audio")[0];
+       var audioSrc = ctx.createMediaElementSource(audio);
+       var analyser = ctx.createAnalyser();
+
+
+       audioSrc.connect(analyser);
+        frequencyData = new Uint8Array(analyser.frequencyBinCount);
+
+       // loop
+       function render() {
+          requestAnimationFrame(render);
+          analyser.getByteFrequencyData(frequencyData);
+
+          //console.log(frequencyData);
+          moveCube(frequencyData);
+       }
+       audio.play();
+       render();
+     };
