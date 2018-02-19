6e8fef0bedaa261c831daa625d3862241f55249a
DranoelMit <tim.matthew.leonard@gmail.com>
2018-02-18 21:08:48 -0500

    testing branch

--- public/JS/SoundGrabber.js
+++ public/JS/SoundGrabber.js
@@ -3,23 +3,22 @@ function start(choice) {
 
      var audio = document.getElementsByClassName(choice)[0];
      var ctx = new AudioContext(); //Does not work with IOS browsers
-
-     var audioSrc = ctx.createMediaElementSource(audio);
-     var analyser = ctx.createAnalyser();
+          var audioSrc = ctx.createMediaElementSource(audio);
+          var analyser = ctx.createAnalyser();
 
 
        audioSrc.connect(analyser);
         frequencyData = new Uint8Array(analyser.frequencyBinCount);
         spawn();
+        playMusic(choice);
 
        // loop
        function render() {
           requestAnimationFrame(render);
           analyser.getByteFrequencyData(frequencyData);
-
           //console.log(frequencyData);
           update(frequencyData);
        }
-      playMusic(choice);
+
        render();
      }
