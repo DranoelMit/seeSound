624d4025b4128cf4b6969486bebe7ae8a21d2dad
DranoelMit <tim.matthew.leonard@gmail.com>
2018-02-20 17:14:06 -0500

    Finish file upload scripts

--- public/JS/SoundGrabber.js
+++ public/JS/SoundGrabber.js
@@ -23,59 +23,12 @@ function start(choice) {
        render();
      };
 
-var soundUp = document.getElementById("songUpload");
-soundUp.onchange = function(){
-     var sounds = document.getElementsByClassName('audiotag4');
-     var reader = new FileReader();
-     reader.onload = function(e) {
-          let x = this.result;
-          sounds[0].src = x;
-          sounds[1].src = x;
-    };
-    reader.readAsDataURL(this.files[0]);
-     start("audiotag4");
+     var soundUp = document.getElementById("songUpload");
+     soundUp.onchange =  function(e){
+     var sounds = document.getElementsByClassName("audiotag4");
+  let x = window.URL.createObjectURL(this.files[0]);
+     sounds[0].src = x;
+     sounds[1].src =x;
+
+start("audiotag4");
 }
-
-//
-// var sound1 = document.getElementById('a41');
-// var sound2 = document.getElementById('a42');
-//
-//   var reader = new FileReader();
-//   reader.onload = function(e) {
-//     sound1.src = this.result;
-//     sound2.src = this.result;
-//
-//
-//     };
-//   reader.readAsDataURL(soundFile.files[0]);
-//   start('audiotag4');
-// }
-//      //     // FileReader support
-//      //     if (songFile){
-     //
-     //              let x = URL.createObjectURL(songFile);
-     //             document.getElementById("a41").setAttribute("src", x);
-     //             document.getElementById("a42").setAttribute("src", x);
-     //
-     //         start('audiotag4');
-     //     }
-     //
-     //     // Not supported
-     //     else {
-     //         // fallback -- perhaps submit the input to an iframe and temporarily store
-     //         // them on the server until the user's session ends.
-     //     }
-     // }
-
-//
-// function fileCheck(songFile){
-// console.log(songFile);
-//      var audio4s = document.getElementsByClassName("audiotag4");
-//
-//      for(i=0; i<audio4s.length; i++){
-//           var source = document.createElement("source");
-//                source.setAttribute("src", songFile.value);
-//           audio4s[i].appendChild(source);
-//      }
-//      start("audiotag4");
-// }
