06629fe8b78fe09c8035e998304a8fd3e60d95bd
Ookayleb <cchoy@stevens.edu>
2018-02-20 20:08:17 -0500

    TAB
    tab tab

--- public/JS/DispSound.js
+++ public/JS/DispSound.js
@@ -1,76 +1,75 @@
-function setBackground(choice)
-{
-     let choices ={
-          'audiotag1' : "Gradient1.jpg",
-          'audiotag2' : "Gradient2.jpg",
-          'audiotag3' : "Gradient3.jpg",
-          'audiotag4' : "Gradient4.jpg"
-     };
-     document.getElementById('background').setAttribute("src", "./images/" +choices[choice] );
+function setBackground(choice){
+    let choices ={
+        'audiotag1' : "Gradient1.jpg",
+        'audiotag2' : "Gradient2.jpg",
+        'audiotag3' : "Gradient3.jpg",
+        'audiotag4' : "Gradient4.jpg"
+    };
+    document.getElementById('background').setAttribute("src", "./images/" +choices[choice] );
 }
 
 function playMusic(choice){
-     setBackground(choice);
-     var audios = document.getElementsByClassName(choice);
-     audios[0].play();
-     audios[1].play();
-     document.getElementById('warn').style.display = "none";
-     document.getElementById('default_music').style.display = "none";
-     document.getElementById('MusicUpload').style.display = "none";
+    setBackground(choice);
+    var audios = document.getElementsByClassName(choice);
+    audios[0].play();
+    audios[1].play();
+    document.getElementById('warn').style.display = "none";
+    document.getElementById('default_music').style.display = "none";
+    document.getElementById('MusicUpload').style.display = "none";
 }
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 function spawn(){
-     var scene = document.getElementsByTagName("a-scene")[0];
-     const numBars =100;
-     for(i=0; i<numBars; i++){
-          let x = document.createElement("a-box");
-          x.setAttribute("class", "soundBars");
-          //54 is just the distance from the camera that looks nice. 6.283 is 2PI  #magicNumbers
-          x.setAttribute("position", ""+(27*Math.cos(i*6.28319/numBars))+" "+1.6+" "+(27*Math.sin(i*6.28319/numBars)));
-          x.setAttribute("color", getRandomColor());
-          scene.appendChild(x);
-     }
-     const numCubes = 15;
-     for(i=0; i<numCubes; i++){
-          let y = document.createElement("a-box");
-          y.setAttribute("class", "spazCube");
-          y.setAttribute("position", ""+(160*(Math.cos(i*6.28319/numCubes)))+" "+75+" "+(160*Math.sin(i*6.28319/numCubes)));
-          y.setAttribute("rotation", ""+8*i+ " "+10*i+ " " + 12*i);
-          y.setAttribute("scale",  "1 1 1");
-          y.setAttribute("color", getRandomColor());
-          scene.appendChild(y);
+    var scene = document.getElementsByTagName("a-scene")[0];
+    const numBars =100;
+    for(i=0; i<numBars; i++){
+        let x = document.createElement("a-box");
+        x.setAttribute("class", "soundBars");
+        //54 is just the distance from the camera that looks nice. 6.283 is 2PI  #magicNumbers
+        x.setAttribute("position", ""+(27*Math.cos(i*6.28319/numBars))+" "+1.6+" "+(27*Math.sin(i*6.28319/numBars)));
+        x.setAttribute("color", getRandomColor());
+        scene.appendChild(x);
+    }
+    const numCubes = 15;
+    for(i=0; i<numCubes; i++){
+        let y = document.createElement("a-box");
+        y.setAttribute("class", "spazCube");
+        y.setAttribute("position", ""+(160*(Math.cos(i*6.28319/numCubes)))+" "+75+" "+(160*Math.sin(i*6.28319/numCubes)));
+        y.setAttribute("rotation", ""+8*i+ " "+10*i+ " " + 12*i);
+        y.setAttribute("scale",  "1 1 1");
+        y.setAttribute("color", getRandomColor());
+        scene.appendChild(y);
 
-          let z = document.createElement("a-box");
-          z.setAttribute("class", "spazCube");
-          z.setAttribute("position", ""+(80*(Math.cos(i*6.28319/numCubes)))+" "+100+" "+(80*Math.sin(i*6.28319/numCubes)));
-          z.setAttribute("rotation", ""+8*i+ " "+10*i+ " " + 12*i);
-          z.setAttribute("scale",  "1 1 1");
-          z.setAttribute("color", getRandomColor());
-          scene.appendChild(z);
-     }
+        let z = document.createElement("a-box");
+        z.setAttribute("class", "spazCube");
+        z.setAttribute("position", ""+(80*(Math.cos(i*6.28319/numCubes)))+" "+100+" "+(80*Math.sin(i*6.28319/numCubes)));
+        z.setAttribute("rotation", ""+8*i+ " "+10*i+ " " + 12*i);
+        z.setAttribute("scale",  "1 1 1");
+        z.setAttribute("color", getRandomColor());
+        scene.appendChild(z);
+    }
 }
 
 function update(frequencyData){
-//there is 1024 indexes in the array
-     var bars = document.getElementsByClassName("soundBars");
-     for(i=0; i<bars.length; i++){
-          bars[i].setAttribute("scale", "1 "+  ((3+(frequencyData[i]))/20) + " 1");
-     }
+    //there is 1024 indexes in the array
+    var bars = document.getElementsByClassName("soundBars");
+    for(i=0; i<bars.length; i++){
+        bars[i].setAttribute("scale", "1 "+  ((3+(frequencyData[i]))/20) + " 1");
+    }
 
-     var cubes = document.getElementsByClassName("spazCube");
-     for(i=0; i<cubes.length; i++){
+    var cubes = document.getElementsByClassName("spazCube");
+    for(i=0; i<cubes.length; i++){
 
-          cubes[i].setAttribute("scale", ""+(Math.sqrt(frequencyData[i]))+" "
-                                           +(Math.sqrt(frequencyData[i*5]))+" "
-                                           +(Math.sqrt(frequencyData[i*10])));
-     }
+        cubes[i].setAttribute("scale", ""+(Math.sqrt(frequencyData[i]))+" "
+                                       +(Math.sqrt(frequencyData[i*5]))+" "
+                                       +(Math.sqrt(frequencyData[i*10])));
+    }
 };
--- public/JS/SoundGrabber.js
+++ public/JS/SoundGrabber.js
@@ -1,34 +1,34 @@
 
 function start(choice) {
-     var ctx = new AudioContext();
-       var audio = document.getElementsByClassName(choice)[0];
-       var audioSrc = ctx.createMediaElementSource(audio);
-       var analyser = ctx.createAnalyser();
+    var ctx = new AudioContext();
+    var audio = document.getElementsByClassName(choice)[0];
+    var audioSrc = ctx.createMediaElementSource(audio);
+    var analyser = ctx.createAnalyser();
 
 
-       audioSrc.connect(analyser);
-        frequencyData = new Uint8Array(analyser.frequencyBinCount);
+    audioSrc.connect(analyser);
+    frequencyData = new Uint8Array(analyser.frequencyBinCount);
 
-        spawn();
+    spawn();
 
-       // loop
-       function render() {
-          requestAnimationFrame(render);
-          analyser.getByteFrequencyData(frequencyData);
+    // loop
+    function render() {
+        requestAnimationFrame(render);
+        analyser.getByteFrequencyData(frequencyData);
 
-          //console.log(frequencyData);
-          update(frequencyData);
-       }
-       playMusic(choice);
-       render();
-     };
+        //console.log(frequencyData);
+        update(frequencyData);
+    }
+    playMusic(choice);
+    render();
+};
 
-     var soundUp = document.getElementById("songUpload");
-     soundUp.onchange =  function(e){
-     var sounds = document.getElementsByClassName("audiotag4");
-  let x = window.URL.createObjectURL(this.files[0]);
-     sounds[0].src = x;
-     sounds[1].src =x;
+var soundUp = document.getElementById("songUpload");
+soundUp.onchange =  function(e){
+    var sounds = document.getElementsByClassName("audiotag4");
+    let x = window.URL.createObjectURL(this.files[0]);
+    sounds[0].src = x;
+    sounds[1].src =x;
 
-start("audiotag4");
+    start("audiotag4");
 }
