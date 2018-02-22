ffe85e5dc0a21ea5399acc2387499de362d07a89
Tim Leonard <tim.matthew.leonard@gmail.com>
2018-02-22 09:50:12 -0500

    re route express server to new folder name

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var app = express();
 var server = require("http").createServer(app);
 
 server.listen(process.env.PORT || 8080);
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/docs"));
 
 
 app.get("/", function(req, res){
-	res.sendFile(__dirname + "/public/index.html");
+	res.sendFile(__dirname + "/docs/index.html");
     });
